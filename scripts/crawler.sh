fe705cc136b72603ac57289cf1da78df86f90495
abstrct <abstrct@slow.ninja>
2025-03-21 01:45:59 -0400

    Base crawler logic

--- scripts/crawler.sh
+++ scripts/crawler.sh
@@ -8,8 +8,25 @@ while :
 do
   # Get latest guild updates
   PENDING_TRANSACTION_COUNT=$(psql $DATABASE_URL -c "SELECT COUNT(1) FROM signer.tx WHERE status = 'pending';" --no-align -t)
-
   echo "CRAWLER: ${PENDING_TRANSACTION_COUNT} Pending Transactions "
+  CRAWL_JSON=$(psql $DATABASE_URL -c 'select structs.GET_GUILD_UPDATES;' --no-align -t)
+  CRAWL_GUILD_ID=$(echo $STUB_ACCOUNT_JSON | jq -r '.id')
 
-  sleep $STAT_SLEEP
+  if [[ ! -z "$CRAWL_JSON" ]]; then
+    if [ "$CRAWL_GUILD_ID" != "null" ]; then
+      echo $CRAWL_JSON > /var/structs/tmp/crawl_${CRAWL_GUILD_ID}.json
+
+      echo "CRAWLER ($BASHPID): Performing Crawl of ${CRAWL_GUILD_ID}"
+
+      # Curl endpoint
+
+      # Update Guild
+
+    else
+        sleep $STAT_SLEEP
+    fi
+  else
+      sleep $STAT_SLEEP
+  fi
+
 done
